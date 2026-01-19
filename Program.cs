3c67a5174b8203d01b575f0267bcc62dc8d2b1b2
user <user@users-MacBook-Pro.local>
2026-01-20 06:11:21 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 5;
+int b = 10;
+int sum = a + b;
+int mul = a * b;
+int div = b / a;
+int sub = b - a;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Multiply: " + mul);
+Console.WriteLine("Divide: " + div);
+Console.WriteLine("Subtract: " + sub);
+
+Bring.Ifworks();
